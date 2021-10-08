bb311b8d0f64c22750024b793acb4048e8fb7783
gadingg <40866806+gadingg@users.noreply.github.com>
2021-10-08 13:42:32 +0700

    Add files via upload

---
+++ baseplus_commision.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Inheritancee2
+{
+    public class KomisiKaryawanDasarPlus
+    {
+        public string NamaDepan { get; }
+        public string NamaBelakang { get; }
+        public string NomorKTP { get; }
+        private decimal penjualanKotor;
+        private decimal tingkatKomisi;
+        private decimal gajiPokok;
+
+        public KomisiKaryawanDasarPlus(string namaDepan, string namaBelakang, string nomorKTP, decimal penjualanKotor, decimal tingkatKomisi, decimal gajiPokok)
+        {
+            NamaDepan = namaDepan;
+            NamaBelakang = namaBelakang;
+            NomorKTP = nomorKTP;
+            PenjualanKotor = penjualanKotor;
+            TingkatKomisi = tingkatKomisi;
+            GajiPokok = gajiPokok;
+        }
+        public decimal PenjualanKotor
+        {
+            get
+            {
+                return penjualanKotor;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(PenjualanKotor)} harus >= 0");
+                }
+                penjualanKotor = value;
+            }
+        }
+        public decimal TingkatKomisi
+        {
+            get
+            {
+                return tingkatKomisi;
+            }
+            set
+            {
+                if (value <= 0 || value >= 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(TingkatKomisi)} harus > 0 dan < 1");
+                }
+                tingkatKomisi = value;
+            }
+        }
+
+        public decimal GajiPokok
+        {
+            get
+            {
+                return gajiPokok;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(GajiPokok)}");
+                }
+                gajiPokok = value;
+            }
+        }
+
+        public decimal Pendapatan() => gajiPokok + (tingkatKomisi * penjualanKotor);
+
+        public override string ToString() =>
+            $" Gaji pokok komisi karyawan: {NamaDepan} {NamaBelakang}\n" +
+            $" Nomor KTP: {NomorKTP}\n" +
+            $" Penjualan kotor: {penjualanKotor:C}\n" +
+            $" Pingkat komisi: {tingkatKomisi:F2}\n" +
+            $" Gaji pokok: {gajiPokok:C}";
+    }
+    class TesKomisiKaryawanDasarPlus
+    {
+        static void Main()
+        {
+            var karyawan = new KomisiKaryawanDasarPlus("Bob", "Lewis", "333-33-3333", 5000.00M, .04M, 300.00M);
+
+            Console.WriteLine(" -- Informasi Karyawan diperoleh dengan properti dan metode : -- \n");
+            Console.WriteLine($" Nama depan : {karyawan.NamaDepan}");
+            Console.WriteLine($" Nama belakang : {karyawan.NamaBelakang}");
+            Console.WriteLine($" Nomor KTP : {karyawan.NomorKTP}");
+            Console.WriteLine($" Penjualan kotor : {karyawan.PenjualanKotor:C}");
+            Console.WriteLine($" Tingkat komisi : {karyawan.TingkatKomisi:F2}");
+            Console.WriteLine($" Pendapatan : {karyawan.Pendapatan():C}");
+            Console.WriteLine($" Gaji Pokok : {karyawan.GajiPokok:C}");
+
+            karyawan.GajiPokok = 1000.00M;
+            Console.WriteLine("\n -- Informasi Karyawan diperoleh dengan properti dan metode : --\n");
+            Console.WriteLine(karyawan);
+            Console.WriteLine($" Pendapatan: {karyawan.Pendapatan():C}");
+        }
+    }
+}
