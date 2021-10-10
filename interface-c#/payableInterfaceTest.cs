03ebe2c23c931416bed90276a3475b0247151da3
gadingutama <40866806+gadingg@users.noreply.github.com>
2021-10-10 21:24:29 +0700

    Add files via upload

---
+++ interface-c#/payableInterfaceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface_Ipayable
+{
+    class PayableInterfaceTest
+    {
+        static void Main(string[] args)
+        {
+            var objekHutang = new List<Ipayable>()
+            {
+                new Invoice("01234", "seat", 2, 375.00M),
+                new Invoice("56789", "tire", 4, 79.95M),
+                new KaryawanBergaji("John", "Smith", "111-11-1111", 800.00M),
+                new KaryawanBergaji("Lisa", "Barnes", "888-88-8888", 1200.00M)
+            };
+
+            Console.WriteLine("Faktur dan Karyawan diproses secara polimorfis:\n");
+            foreach (var hutang in objekHutang)
+            {
+                Console.WriteLine($"{hutang}");
+                Console.WriteLine($"tanggal jatuh tempo: {hutang.DapatkanJumlahPembayaran():C}\n");
+            }
+        }
+    }
+}
